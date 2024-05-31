f342ba839618879fa939bdbb563811946371b076
Arthur Almeida <arthuraraujo07@hotmail.com>
2024-05-31 17:36:51 -0300

    commit ugprades: excel/navbar/etc

--- alunos/urls.py
+++ alunos/urls.py
@@ -1,7 +1,5 @@
-# alunos/urls.py
-
 from django.urls import path
-from .views import home, turma_list, aluno_add, horario_add, turma_add, presenca_add, sobre, professores
+from .views import home, turma_list, aluno_add, horario_add, turma_add, presenca_add, sobre, professores, save_presences_matutina, save_presences_vespertina, export_presences_to_excel, desempenho_presencas, alunos_atendidos_por_mes
 
 urlpatterns = [
     path('', home, name='home'),
@@ -12,4 +10,9 @@ urlpatterns = [
     path('presenca/<int:aluno_id>/add/', presenca_add, name='presenca_add'),
     path('sobre/', sobre, name='sobre'),
     path('professores/', professores, name='professores'),
+    path('save_presences_matutina/', save_presences_matutina, name='save_presences_matutina'),
+    path('save_presences_vespertina/', save_presences_vespertina, name='save_presences_vespertina'),
+    path('export_presences_to_excel/', export_presences_to_excel, name='export_presences_to_excel'),
+    path('desempenho_presencas/', desempenho_presencas, name='desempenho_presencas'),
+    path('alunos_atendidos_por_mes/', alunos_atendidos_por_mes, name='alunos_atendidos_por_mes'),
 ]
